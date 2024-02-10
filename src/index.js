64e06a06a3ae2c039a8b975ca0471a4576011a77
jetzerszeri <jetzers.2@gmail.com>
2024-02-10 17:49:52 -0500

    feat: import and use the BrowserRouter from react-router-dom on the index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './css/styles.css';
 import { SniffNearApp } from './SniffNearApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SniffNearApp />
+    <BrowserRouter>
+      <SniffNearApp />
+    </BrowserRouter>
   </React.StrictMode>
 );

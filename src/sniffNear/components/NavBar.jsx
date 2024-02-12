c3b6abdb125635fbfc3875d4e78768091b9e5c38
jetzerszeri <jetzers.2@gmail.com>
2024-02-12 16:05:12 -0500

    feat: show and hide sidebar navigation

--- src/sniffNear/components/NavBar.jsx
+++ src/sniffNear/components/NavBar.jsx
@@ -1,28 +1,65 @@
-import { Link } from "react-router-dom"
-import { SideBar } from "./SideBar"
-import { SniffNearLogotipo } from "../../ui/customIcons"
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SideBar } from './SideBar';
+import { SniffNearLogotipo } from '../../ui/customIcons';
 
 
 export const NavBar = () => {
-  return (
-    <>
-        <div className="topNavBar">
-            <div>
-                <i className="bi bi-list menubtn"></i>
+
+    const [displaySidebar, setDisplaySidebar] = useState({
+        background: false,
+        nav: false,
+        backTranslateX: false
+    });
+
+    const showSidebar = () => {
+        setDisplaySidebar({
+            background: true,
+            nav: true,
+            backTranslateX: true
+        })
+    };
+
+    const hideSidebar = () => {
+        setDisplaySidebar({
+            ...displaySidebar,
+            background: false,
+            nav: false
+
+        })
+
+        setTimeout(() => {
+            setDisplaySidebar({
+                backTranslateX: false,
+                background: false,
+                nav: false,
+            })
+        }, 500);
+    };
+
+    return (
+        <>
+            <div className="topNavBar">
+                <div>
+                    <i 
+                        className="bi bi-list menubtn"
+                        onClick={ showSidebar }
+                    ></i>
+                </div>
+                <div className="logoTopBar">
+                <Link to='/'>
+                    <SniffNearLogotipo />
+                </Link> 
+                </div>
+                <div>
+                
+                {/* <Link to={"/user-profile?id=${userIdTest}"}> */}
+                <i className="bi bi-person-fill"></i>
+                {/* </Link> */}
+                </div>
             </div>
-            <div className="logoTopBar">
-            <Link to='/'>
-                <SniffNearLogotipo />
-            </Link> 
-            </div>
-            <div>
-            
-            {/* <Link to={"/user-profile?id=${userIdTest}"}> */}
-            <i className="bi bi-person-fill"></i>
-            {/* </Link> */}
-            </div>
-        </div>
-        <SideBar />
-    </>
-  )
-}
+            <SideBar displaySidebar={displaySidebar} hideSidebar={hideSidebar}/>
+        </>
+    )
+    
+};
--- src/sniffNear/components/SideBar.jsx
+++ src/sniffNear/components/SideBar.jsx
@@ -2,55 +2,66 @@ import { Link, NavLink } from 'react-router-dom';
 import { SidebarBlogIcon, SidebarChatIcon, SidebarHomeIcon, SidebarLogoutIcon, SniffNearLogotipo } from '../../ui/customIcons';
 
 
-export const SideBar = () => {
-  return (
-    <div className="sidebar">
-        <nav>
-            <SniffNearLogotipo />
-            <i className="bi bi-x-lg closebtn"></i>
+export const SideBar = ( { displaySidebar, hideSidebar } ) => {
 
-            <ul>
-                <li>
-                    <NavLink 
-                        className={ ({isActive}) => `${isActive ? 'active' : ''}`}
-                        to="/"
-                    >
-                        <SidebarHomeIcon />
-                    </NavLink>
-                </li>
+    const sidebarBackgroundStyle = {
+        transform: displaySidebar.backTranslateX ? 'translateX(0)' : 'translateX(-100%)'
+    }
 
-                <li>
-                    <NavLink 
-                        className={ ({isActive}) => `${isActive ? 'active' : ''}`}
-                        to="/blog"
-                    >
-                        <SidebarBlogIcon />
-                    </NavLink>
-                </li>
+    return (
+        <div 
+            className={`sidebar ${displaySidebar.background ? 'show' : ''}`}
+            style={ sidebarBackgroundStyle }
+            onClick={ hideSidebar }
+        >
+            <nav
+                className={`${displaySidebar.nav ? 'show' : ''}`} 
+                onClick={ (e) => e.stopPropagation() }           
+            >
+                <SniffNearLogotipo />
+                <i 
+                    className="bi bi-x-lg closebtn"
+                    onClick={ hideSidebar }
+                ></i>
 
-                <li>
-                    <NavLink 
-                        className={ ({isActive}) => `${isActive ? 'active' : ''}`}
-                        to="/chat"
-                    >
-                        <SidebarChatIcon />
-                    </NavLink>
-                </li>
+                <ul>
+                    <li>
+                        <NavLink 
+                            className={ ({isActive}) => `${isActive ? 'active' : ''}`}
+                            to="/"
+                            onClick={ hideSidebar }
+                        >
+                            <SidebarHomeIcon />
+                        </NavLink>
+                    </li>
 
-                <li>
-                    <Link to="/login">
-                        <SidebarLogoutIcon />
-                    </Link>
-                </li>
+                    <li>
+                        <NavLink 
+                            className={ ({isActive}) => `${isActive ? 'active' : ''}`}
+                            to="/blog"
+                            onClick={ hideSidebar }
+                        >
+                            <SidebarBlogIcon />
+                        </NavLink>
+                    </li>
 
+                    <li>
+                        <NavLink 
+                            className={ ({isActive}) => `${isActive ? 'active' : ''}`}
+                            to="/chat"
+                            onClick={ hideSidebar }
+                        >
+                            <SidebarChatIcon />
+                        </NavLink>
+                    </li>
 
-
-
-
-            </ul>
-
-
-        </nav>
-    </div>
-  )
+                    <li>
+                        <Link to="/login">
+                            <SidebarLogoutIcon />
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }

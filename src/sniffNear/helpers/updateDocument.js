6198fca7ab2106875cfcc3ea311d06de5eb831bd
jetzerszeri <jetzers.2@gmail.com>
2024-02-15 19:20:42 -0500

    refactor: import fetchSniffNearApi on UpdateDocument.js helper

--- src/sniffNear/helpers/updateDocument.js
+++ src/sniffNear/helpers/updateDocument.js
@@ -1,21 +1,8 @@
+import { fetchSniffNearApi } from "../../helpers"
+
 export const updateDocument = async ( collection, id, data ) => {
-    try {
-        const response = await fetch(`https://sniffnear-api.onrender.com/api/${collection}/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
 
-        const json = await response.json();
+    const fetchResponse = await fetchSniffNearApi(`${collection}/${id}`, 'PUT', data);
 
-        if (response.ok) {
-            return json;
-        } else {
-            return { error: json.message };
-        }
-    } catch (e) {
-        return { error: e.message };
-    }
+    return fetchResponse;
 }

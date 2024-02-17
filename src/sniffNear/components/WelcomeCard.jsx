aeb61af01fd64d8eb20fa3e8307eec3a1f0aae44
jetzerszeri <jetzers.2@gmail.com>
2024-02-17 11:39:17 -0500

    refactor: WelcomeCard on home to fetch user info on the backend

--- src/sniffNear/components/WelcomeCard.jsx
+++ src/sniffNear/components/WelcomeCard.jsx
@@ -1,9 +1,35 @@
+import { useContext, useEffect } from "react"
+import { useFetchSniffNearApi } from "../../hooks"
+import { AuthContext } from "../../auth/context/AuthContext"
 
 
-export const WelcomeCard = ( { name, location='Ubicación no encontrada', imgUrl } ) => {
+export const WelcomeCard = ( { location='Ubicación no encontrada', imgUrl } ) => {
+
+    const { user } = useContext( AuthContext );
+    const { data, getData  } = useFetchSniffNearApi();
+
+    
+    useEffect(() => {
+        getData(`users/${user.id}`);
+        // console.log(data);
+    }, [ ]);
+
+    const { name, profileImg } = !!data && data.user;
+    
+    // getData(`users/${user.id}`);
+
+    // useEffect(() => {
+    //     console.log(data);
+    // }, [ data ])
+    
+
     return (
         <section className="welcomeCard">
-            <img src={ imgUrl } alt="Avatar del usuario"/>
+            <img src={ 
+                ( !!profileImg ) 
+                ? profileImg : 
+                '/img/avatarPorDefecto.webp'    
+        } alt="Avatar del usuario"/>
             <div>
                 <h1>¡Hola, {name}!</h1>
                 <p>
--- src/sniffNear/pages/HomePage.jsx
+++ src/sniffNear/pages/HomePage.jsx
@@ -3,13 +3,12 @@ import { NavBar } from "../components/NavBar"
 import { WelcomeCard } from "../components/WelcomeCard"
 import { WelcomeCardNoUserLogged } from "../components/WelcomeCardNoUserLogged"
 import { AuthContext } from "../../auth/context"
+// import { Loader } from "../../ui"
 
 
 export const HomePage = () => {
 
   const { user, isLogged } = useContext(AuthContext);
-
-  const imgUrl = user.profileImg ? user.profileImg : '/img/avatarPorDefecto.webp';
 
   return (
     <>
@@ -18,10 +17,12 @@ export const HomePage = () => {
       <main>
         {
           isLogged 
-            ? <WelcomeCard name={user.name} imgUrl={ imgUrl } />
+            ? <WelcomeCard />
             : <WelcomeCardNoUserLogged />
         }
       </main>
+
+      {/* <Loader label="Cargando..." /> */}
     </>
   )
 }

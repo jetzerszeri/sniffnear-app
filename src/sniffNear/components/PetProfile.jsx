d466eb548838dbf4d5d6fd023ae9073ed7f96164
jetzerszeri <jetzers.2@gmail.com>
2024-02-22 18:51:37 -0500

    refactor: display delete pet profile modal when click on delete btn

--- src/sniffNear/components/PetProfile.jsx
+++ src/sniffNear/components/PetProfile.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
 import { calculateAge } from '../../helpers';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../auth/context';
 import { Modal } from '../../ui';
 
 export const PetProfile = ( { pet }) => {
 
     const { user } = useContext( AuthContext );
+    const [ showModal, setShowModal] = useState( false );
+    const { name, breed, breedType, birthdate, type, size, color1, img, sex, _id } = pet;
 
-    const { name, breed, breedType, birthdate, type, size, color1, img, sex, _id } = pet
+    const displayModal = () => {
+        setShowModal( !showModal );
+    }
+
+
 
 
     return (
@@ -36,18 +42,20 @@ export const PetProfile = ( { pet }) => {
                 {
                     user.id === pet.owner &&
                     <div className='actions'>
-                        <button className='btn secundary'>Eliminar</button>
+                        <button className='btn secundary' onClick={ displayModal }>Eliminar</button>
                         <Link className='btn' to={`/pets/${_id}/edit`}>Editar</Link>
                     </div>
                 }
 
             </div>
 
-
-            <Modal text={`¿Estás seguro de querer eliminar el perfil de ${ name }?`} type='danger' icon={ true }>
-                <button className="btn secundary">Cancelar</button>
-                <button className="btn">Si, Eliminar</button>
-            </Modal>
+            {
+                showModal &&
+                <Modal text={`¿Estás seguro de querer eliminar el perfil de ${ name }?`} type='danger' icon={ true }>
+                    <button className="btn secundary" onClick={ displayModal }>Cancelar</button>
+                    <button className="btn">Si, Eliminar</button>
+                </Modal>
+            }
 
         </div>
     )

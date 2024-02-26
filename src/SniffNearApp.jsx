71216acd9d0a7dc609651e691fdf57dabd9319fa
jetzerszeri <jetzers.2@gmail.com>
2024-02-25 20:00:09 -0500

    feat: add googleMaps Api provider on the top component

--- src/SniffNearApp.jsx
+++ src/SniffNearApp.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import { AuthProvider } from './auth/context';
 import { AppRouter } from './router/AppRouter';
 import { SplashScreen } from './ui';
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 
 export const SniffNearApp = () => {
 
   const [ displaySC, setDisplaySC ] = useState( true );
+  const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
   return (
     <AuthProvider>
-        { displaySC && <SplashScreen  toggle={setDisplaySC}/> }
+      { displaySC && <SplashScreen  toggle={setDisplaySC}/> }
+
+      <APIProvider apiKey={googleMapsApiKey}>
         <AppRouter />
+      </APIProvider>
     </AuthProvider>
   )
 }

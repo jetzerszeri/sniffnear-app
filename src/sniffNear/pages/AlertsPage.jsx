3340875d26128dad39537c2f226d316943e714be
jetzerszeri <jetzers.2@gmail.com>
2024-02-28 11:01:05 -0500

    feat: create cards to display on alert list

--- src/sniffNear/pages/AlertsPage.jsx
+++ src/sniffNear/pages/AlertsPage.jsx
@@ -9,6 +9,8 @@ import {
   } from "@vis.gl/react-google-maps";
 import { AuthContext } from '../../auth/context';
 import { useFetchSniffNearApi } from '../../hooks';
+import { AlertIcon, FoundIcon } from '../../ui';
+import { Link } from 'react-router-dom';
 
 export const AlertsPage = () => {
 
@@ -39,6 +41,7 @@ export const AlertsPage = () => {
 
     
     // console.log(coords);
+    const imglink = "https://firebasestorage.googleapis.com/v0/b/sniffnear.appspot.com/o/Willem_20231106_094408.jpeg?alt=media&token=edaa1025-9bc2-492a-8659-098846b3d8ca";
 
 
     return (
@@ -47,11 +50,80 @@ export const AlertsPage = () => {
 
 
             {
-                position &&
-                <MapSniffNear position={position} />
+                // position &&
+                // <MapSniffNear position={position} />
             }
 
+            {/* <div className="alertCard1">
+                <img src={imglink}  alt="" />
+                <div>
+                    <div className='h'>
+                        <AlertIcon />
+                        <div>
+                            <h2>Perro perdido</h2>
+                            <p>Maltese | Color café, tamaño mediano.</p>
+                        </div>
+                    </div>
 
+                    <div>
+                        <p>Visto por última vez a 5km de distancia de aquí.</p>
+                        <p>Es juguetón, anda con un collar rojo</p>
+                        <Link to='/alerts/1' className='link'>Ver más</Link>
+                    </div>
+                </div>
+            </div> */}
+
+            <ul className='alertList'>
+
+            
+            <li className="alertCard perdido">
+                <div className='h'>
+                        <AlertIcon />
+                        <div>
+                            <h2>Perro perdido</h2>
+                            <p><i className="bi bi-geo-alt"></i>Visto por última vez a 5km de distancia.</p>
+                        </div>
+                </div>
+
+
+                <div>
+                    <img src={imglink}  alt="" />
+
+                    <div>
+                        <p>Maltese | Color café, tamaño mediano.</p>
+                        <p>Es juguetón, anda con un collar rojo.</p>
+                        <div className='actions'>
+                            <Link to='/alerts/1' className='btn small secundary'>Ver más</Link>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+
+
+            <li className="alertCard encontrado">
+                <div className='h'>
+                        <FoundIcon />
+                        <div>
+                            <h2>Perro encontrado</h2>
+                            <p><i className="bi bi-geo-alt"></i>A 5km de distancia.</p>
+                        </div>
+                </div>
+
+
+                <div>
+                    <img src={imglink}  alt="" />
+
+                    <div>
+                        <p>Maltese | Color café, tamaño mediano.</p>
+                        <p>Es juguetón, anda con un collar rojo.</p>
+                        <div className='actions'>
+                            <Link to='/alerts/1' className='btn small secundary'>Ver más</Link>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            </ul>
 
 
 

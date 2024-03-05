4bfd7e52b116eb19be52dc611ba94c3b35cf7d4c
ailenad <ailen.alvarez@davinci.edu.ar>
2024-03-05 09:28:48 -0300

    InboxPage + css

--- src/sniffNear/pages/InboxPage.jsx
+++ src/sniffNear/pages/InboxPage.jsx
@@ -6,7 +6,7 @@ export const InboxPage = () => {
   //logica del inbox
   const { user } = useContext( AuthContext );
   const [chats, setChats] = useState([]);
-  const [sender, setSender] = useState({});
+  const [senders, setsenders] = useState({});
   const userId= user.id;
 
   useEffect(()=>{
@@ -35,49 +35,51 @@ export const InboxPage = () => {
 
   }, [userId]);
   
-  const updateSenderNames = async (chats, userId) => {
-    const newSenders = {};
+  const updatesendersInfo = async (chats, userId) => {
+    const newsenders = {};
 
     for (const chat of chats) {
 
-        const senderId = chat.participants.find(participant => participant !== userId);
+        const sendersId = chat.participants.find(participant => participant !== userId);
 
-        if (senderId) {
+        if (sendersId) {
           try {
 
-            const response = await fetch(`https://sniffnear-api.onrender.com/api/users/${senderId}`);
+            const response = await fetch(`https://sniffnear-api.onrender.com/api/users/${sendersId}`);
 
             if (response.ok) {
 
-              const data = await response.json();
-              console.log(data)
-              newSenders[chat._id] = data.user.name;
- 
+              const userData = await response.json();
+             
+              newsenders[chat._id] = {
+                name: userData.user.name,
+                profileImg: userData.user.profileImg || '/img/defaultAvatar.png',
+              };
             } else {
-
-              console.error(`Error al obtener el usuario sender: ${response.status}`);
-              newSenders[chat._id] = 'Nombre no disponible';
-
+              console.error(`Error al obtener el usuario senders: ${response.status}`);
+              newsenders[chat._id] = {
+                name: 'Nombre no disponible',
+                profileImg: '/default-avatar.png'
+              };
             }
           } catch (error) {
 
-              console.error('Error al traer el usuario sender:', error);
-              newSenders[chat._id] = 'Nombre no disponible';
+            console.error('Error al traer el usuario senders:', error);
 
           }
         }
     }
 
-    return newSenders;
+    return newsenders;
 
   };
 
   useEffect(() => {
-    const fetchSenderNames = async () => {
+    const fetchsendersInfo = async () => {
       try {
 
-        const updatedSenderNames = await updateSenderNames(chats, userId);
-        setSender(updatedSenderNames);
+        const updatedsendersInfo = await updatesendersInfo(chats, userId);
+        setsenders(updatedsendersInfo);
 
       } catch (error) {
 
@@ -85,7 +87,7 @@ export const InboxPage = () => {
       }
     };
 
-    fetchSenderNames();
+    fetchsendersInfo();
 
   }, [chats, userId]);
 
@@ -97,18 +99,31 @@ return (
       <h1 className='conversation'>Tus conversaciones</h1>
       <div>
       <ul className='message-list'>
-                {chats.map(chat => (
-                    
-                    <Link to={`/inbox/chat/${chat._id}`} key={chat._id} className="message-link" >
-                        <li className='inbox-card'>
-                            <div className='inbox-content'>
-                                <p className='message-username'>{sender[chat._id]}</p>
-                                <i className="bi bi-chat"></i>
-                            </div>
-                        </li>
-                    </Link>
-                ))}
-            </ul>
+          {chats.map(chat => (
+            <Link to={`/inbox/chat/${chat._id}`} key={chat._id} className="message-link" >
+              <li className='inbox-card'>
+                <div className='inbox-content'>
+                    <div className='img-container-chat'>
+                      {senders[chat._id] && senders[chat._id].profileImg && (
+                                        <img
+                                          src={senders[chat._id].profileImg}
+                                          alt="Profile"
+                                          className="profile-image"
+                                        />
+                    )}
+                    </div>
+                   <div className='username-container'>
+                     {senders[chat._id] && (
+                        <p className='message-username'>{senders[chat._id].name}</p>
+                    )}
+                   </div>
+                 
+                    <i className="bi bi-chat"/>
+                  </div>
+                </li>
+              </Link>
+          ))}
+      </ul>
       </div>
     </main>
     <BottomNav/>

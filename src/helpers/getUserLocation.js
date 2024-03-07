feefb88a164c68c224cbfba505875772394d103a
jetzerszeri <jetzers.2@gmail.com>
2024-03-06 19:29:30 -0500

    feat: display modal when location permission is denied

--- src/helpers/getUserLocation.js
+++ src/helpers/getUserLocation.js
@@ -1,18 +1,29 @@
-
-export const getUserLocation = ( setCoords, setError) => {
+export const getUserLocation = (setCoords, setError) => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 setCoords({
                     lat: position.coords.latitude,
-                    lng: position.coords.longitude
+                    lng: position.coords.longitude,
                 });
             },
             (error) => {
-                setError(error.message);
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        setError("Permiso denegado para obtener la ubicación.");
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        setError("La información de ubicación no está disponible.");
+                        break;
+                    case error.TIMEOUT:
+                        setError("La petición para obtener la ubicación ha excedido el tiempo de espera.");
+                        break;
+                    default:
+                        setError("Ha ocurrido un error desconocido al obtener la ubicación.");
+                }
             }
         );
     } else {
-        setError('Ubicación no encontrada.');
+        setError('La geolocalización no es soportada por este navegador.');
     }
 }
--- src/ui/SplashScreen.jsx
+++ src/ui/SplashScreen.jsx
@@ -3,12 +3,13 @@ import { useContext, useEffect, useState } from 'react';
 import { useFetchSniffNearApi, useUserLocation } from '../hooks';
 import { SniffNearMainLogo } from './customIcons';
 import { AuthContext } from '../auth/context';
+import { Modal } from './Modal';
 
 export const SplashScreen = ( { toggle } ) => {
   
   const { coords, address } = useContext( AuthContext );
   const { connectServer } = useFetchSniffNearApi();
-  useUserLocation( true );
+  const { error } = useUserLocation( true );
 
   const [isConnected, setisConnected] = useState('')
 
@@ -26,6 +27,7 @@ export const SplashScreen = ( { toggle } ) => {
   return (
     <div className="splashScreen">
       <SniffNearMainLogo />
+      {error && <p className='inputError'>{error}</p>}
       {/* <p>coordenadas: {JSON.stringify(coords, null, 2)}, conected: {isConnected}, direccion: {JSON.stringify(address, null, 2)}</p> */}
       <div className="bouncer">
           <div></div>
@@ -33,6 +35,24 @@ export const SplashScreen = ( { toggle } ) => {
           <div></div>
           <div></div>
       </div>
+
+      {
+        error &&
+        <Modal
+          heading={error}
+          text={'Es necesario que permitas el acceso a tu ubicación para continuar.'}
+          type='warning'
+        >
+            <div className="bouncer">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+        </Modal>
+      }
+
+      
     </div>
   )
 }
